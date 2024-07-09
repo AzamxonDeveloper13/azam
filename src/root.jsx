4b03ed96e60788b85ccf1ea2aae13e656ea42977
AzamxonDeveloper13 <xodjayevazamxon123@gmail.com>
2024-07-09 09:28:02 +0500

    ASD

---
+++ src/root.jsx
@@ -0,0 +1,17 @@
+/* other imports */
+import Root, { loader as rootLoader } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
