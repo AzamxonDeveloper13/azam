789b67afcb807f2677d6444aec792125bd1b2736
AzamxonDeveloper13 <xodjayevazamxon123@gmail.com>
2024-07-09 09:14:13 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Azamkhon</h1>
+    <Navbar />
+    <h1>Azamkhon</h1>
     </>
   )
 }
